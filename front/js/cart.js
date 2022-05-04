6415ebe1deb6f5a65e195fa171009d653833e31a
Sofiane Idir <idir.sofiane@yahoo.com>
2022-05-04 11:38:33 +0200

    add fonction

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,3 +1,12 @@
+// On récupère les produits stockés dans le local storage
+let addProduits = JSON.parse(localStorage.getItem("produit"));
+const totalProduits = document.querySelector("#totalQuantity");
+const totalPrix = document.querySelector("#totalPrice");
+// Tableaux qui vont contenir les prix / quantité de produits
+let tabQuantite = [];
+let tabPrix = [];
+
+
 // On récupère les données de l'API via fetch
 function getArticle(){
   return fetch(`http://localhost:3000/api/products/`)                           
@@ -13,17 +22,10 @@ function getArticle(){
     alert("le serveur est eteint") 
   })
 }
-
-
-function panierArticle(article) {
-// On récupère les produits stockés dans le local storage
-  let addProduits = JSON.parse(localStorage.getItem("produit"));
-//  Affichage des produits 
+// Fonction pour afficher les produits sur la page panier
+function displayArticle(article) {
 // Sélection de l'id, de la div qui va contenir le contenue des articles
   const carte = document.getElementById("cart__items");
- // On récupère les endroits où l'on souhaite les afficher
-  let totalProduits = document.querySelector("#totalQuantity");
-  let totalPrix = document.querySelector("#totalPrice");
 // Si le panier est vide : 
   if(addProduits === null){
     carte.innerHTML = `<div>Le panier est vide</div>`;
@@ -32,14 +34,13 @@ function panierArticle(article) {
   } 
   else {
 // Si le panier n'est pas vide
-    for(p = 0; p < addProduits.length; p++){
-
+    for(let p = 0; p < addProduits.length; p++){   /// rajout du let p 
       for(let k= 0; k < article.length; k++){
         if(addProduits[p].id === article[k]._id){
           carte.innerHTML += `
           <article class="cart__item" data-id="${addProduits[p].id}" data-color="${addProduits[p].colors}">
             <div class="cart__item__img">
-              <img src="${article[k].imageUrl}" alt="">
+              <img src="${article[k].imageUrl}" alt="${article[k].altTxt}"> 
             </div>
             <div class="cart__item__content">
               <div class="cart__item__content__description">
@@ -62,138 +63,96 @@ function panierArticle(article) {
       }
     }
   }
-/********************* Augmenter la quantiter depuis la page panier ***********************/
-
-// Sélection du bouton supprimé
-  let supprimer = document.querySelectorAll(".deleteItem");
+}
+// Fonction pour augmenter la quantiter dans le local storage depuis la page panier
+function changeQuantityLocalStorage(){
 // Sélection de la div qui contient le produit
-  let carteProduit = document.querySelectorAll(".cart__item")
+  const carteProduit = document.querySelectorAll(".cart__item")
 
-// Exécute une fonction donnée sur chaque élément du tableau
-  carteProduit.forEach((positive)=> {
-  
-    positive.addEventListener("change", (e)=> {
-
-      for(let i = 0; i < addProduits.length; i++){
-        let x = e.target.value;
-// parseInt convertit la chaîne de caractère en nombre
-        let nombreX = parseInt(x);
-
-        if(addProduits[i].id === positive.dataset.id && addProduits[i].colors === positive.dataset.color && x < addProduits[i].quantite && x <= 100 ){
-          return addProduits[i].quantite = nombreX, localStorage.setItem("produit", JSON.stringify(addProduits))
-        } 
-        else if (addProduits[i].id === positive.dataset.id && addProduits[i].colors === positive.dataset.color && x <= 100){
-          return addProduits[i].quantite = nombreX, localStorage.setItem("produit", JSON.stringify(addProduits))
-        }
+  carteProduit.forEach((valueQuantity)=> {
+    valueQuantity.addEventListener("change", (event)=> {
+      for(let i = 0; i < addProduits.length; i++){ 
+        let newValueQuantity = event.target.value;
+        let parseNewValueQuantity = parseInt(newValueQuantity); // renommé
+        // if(addProduits[i].id === valueQuantity.dataset.id && addProduits[i].colors === valueQuantity.dataset.color &&  parseNewValueQuantity < addProduits[i].quantite &&  parseNewValueQuantity <= 100 ){
+        //   return addProduits[i].quantite = parseNewValueQuantity, localStorage.setItem("produit", JSON.stringify(addProduits))
+        // } 
+          if (addProduits[i].id === valueQuantity.dataset.id && addProduits[i].colors === valueQuantity.dataset.color &&  parseNewValueQuantity <= 100){
+           return addProduits[i].quantite = parseNewValueQuantity, localStorage.setItem("produit", JSON.stringify(addProduits))
+         }
       }
     })
   })
+}
+function deletProduct(){
+// Sélection du bouton supprimé
+let supprimer = document.querySelectorAll(".deleteItem");
 // BOUTON SUPPRIMER
   supprimer.forEach((supp) => {
-
+   
     supp.addEventListener("click", function(){
-// Confirmation de la suppression du produit    
+      location.reload();
+// Si l'utilisateur confirme la suppression du produit
       let yes = confirm(" Vous souhaitez retirer ce produit de votre panier ?");
       if(yes){
-
         for(let i = 0; i < addProduits.length; i ++){
-// On rafraîchit le nombre d'articles et le prix après la suppression d'un produit 
-          let produitLog = supp.closest(".cart__item");
-          if(produitLog.dataset.id === addProduits[i].id && produitLog.dataset.color === addProduits[i].colors){
-// Calcul de la quantité après avoir supprimé un article
-            let quantLocal = addProduits[i].quantite;
-            let valueQuant = totalProduits.textContent;
-            let totalValueQuant =  valueQuant - quantLocal;
-            totalProduits.textContent = totalValueQuant;
- 
-// Calcul du prix après avoir supprimé un article 
-            let valuePrice = totalPrix.textContent;
-            let contenuePrice = document.querySelectorAll(".cart__item__content__description p:last-child");
-            let prix = contenuePrice[i].textContent;
-            let nomberPrice = parseInt(prix);
-            let totalValuePrice = valuePrice - nomberPrice;
-            totalPrix.textContent = totalValuePrice;
-          }
-  
-          if(supp.closest(".cart__item").dataset.id === addProduits[i].id && supp.closest(".cart__item").dataset.color === addProduits[i].colors  && addProduits.length > 1){
-            addProduits.splice(i, 1);
+           if(supp.closest(".cart__item").dataset.id === addProduits[i].id && supp.closest(".cart__item").dataset.color === addProduits[i].colors && addProduits.length === 1) {
+             localStorage.removeItem("produit");   
+           } 
+          if(supp.closest(".cart__item").dataset.id === addProduits[i].id && supp.closest(".cart__item").dataset.color === addProduits[i].colors && addProduits.length > 1 ){
+            addProduits.splice(i,1);
             localStorage.setItem("produit", JSON.stringify(addProduits));
-            supp.closest(".cart__item").remove();
-          }
-
-          if(supp.closest(".cart__item").dataset.id === addProduits[i].id && supp.closest(".cart__item").dataset.color === addProduits[i].colors && addProduits.length == 1) {
-            localStorage.removeItem("produit");   
-            supp.closest(".cart__item").remove(); 
-            carte.innerHTML = `<div>Le panier est vide</div>`;
-          }    
+          }   
         }
       }
-      else {
-      localStorage.setItem("produit", JSON.stringify(addProduits));
-      }
+    // else{
+    //   localStorage.setItem("produit", JSON.stringify(addProduits));  // PAS NECESSAIRE JE CROIS
+    //   }
     })
   })
+}
 /************************ Affichage du prix total et de la quantité ************************/
-
-
-// Tableaux qui vont contenir les prix / quantité de produits
-  let tabPrix = [];
-  let tabQuantite = [];
-
-  for (let i = 0; i < carteProduit.length; i++ ){
-
-// Calcul du nombre de produit dans le panier ***************************
-    let contenueQuantite = document.querySelectorAll(".itemQuantity");
-    let quantite = contenueQuantite[i].value;
-
-// Convertie la chaîne de caractère en nombre
+// Fonction qui va calculer la quantité/prix total des produits ajoutés au panier
+function calculTotalQuantitePrix(){
+let quantiteDom = document.querySelectorAll(".itemQuantity");
+  for (let i = 0; i < quantiteDom.length; i++ ){
+    // Calcul de la quantité total
+    let quantite = quantiteDom[i].value;
     let quantNomber = parseInt(quantite);
-// On envoie les prix dans le tableau  
     tabQuantite.push(quantNomber);
-
-// On additionne les valeurs dans le tableau tabQuantite
     const reducer2 = (accumulator, currentValue) => accumulator + currentValue;
     const quantiteToto = tabQuantite.reduce(reducer2, 0);
-// Total de la quantité afficher dans la page panier
     totalProduits.textContent = quantiteToto;
-
-
-// Calcul du prix total des produits dans le panier *************************
+    // Calcul du prix total 
     let contenuePrice = document.querySelectorAll(".cart__item__content__description p:last-child");
     let price = contenuePrice[i].textContent;
-// Convertie la chaîne de caractère en nombre
     let nomber = parseInt(price);
-// RAJOUTER COM + voir placement
     contenuePrice[i].textContent = nomber * quantNomber + "€";
-// On envoie les prix dans le tableau  
     tabPrix.push(nomber * quantNomber);
-// On additionne les valeurs dans le tableau tabPrix
     const reducer = (accumulator, currentValue) => accumulator + currentValue;
     const prixToto = tabPrix.reduce(reducer, 0);
-// Total du prix afficher dans la page panier
     totalPrix.textContent = prixToto;
 
-/******************************** Quantité et prix en fonction des séléctions du client **********************************/
-    carteProduit[i].addEventListener("change", function(e){
-// Total de la quantitée après changement de celle ci 
-      quantite = e.target.value;
+/******************************** total Quantité/prix en fonction des changements **********************************/
+    quantiteDom[i].addEventListener("change", function(event){
+  // Total de la quantité après changement
+      quantite = event.target.value;
       quantNomber = parseInt(quantite);
-      tabQuantite.splice(i, 1, quantNomber);
-      const reducer2 = (accumulator, currentValue) => accumulator + currentValue;
-      const quantiteTotalChange = tabQuantite.reduce(reducer2, 0);
+      tabQuantite.splice(i, 1, quantNomber) 
+      const reducer3 = (accumulator, currentValue) => accumulator + currentValue;
+      const quantiteTotalChange = tabQuantite.reduce(reducer3, 0);
       totalProduits.textContent = quantiteTotalChange;
-
-
+  // Modification du prix d'un seul article en fonction de sa quantité
       let calculPriceProduct = quantNomber * nomber;
-      contenuePrice[i].textContent = calculPriceProduct + "€"
-// Total du prix après changement de la quantité  
+      contenuePrice[i].textContent = calculPriceProduct + "€" 
+  // Total du prix après changement de la quantité  
       let priceChange = contenuePrice[i].textContent;
       let priceChangeNomber = parseInt(priceChange);
       tabPrix.splice(i, 1, priceChangeNomber);
       const reducer = (accumulator, currentValue) => accumulator + currentValue;
       const priceTotalChange = tabPrix.reduce(reducer, 0);
       totalPrix.textContent = priceTotalChange;
-
+  // Si la quantité renseignée dans le dom est supérieur à 100        
       if(quantNomber > 100){
         alert("La quantité total maximum par produit est de 100 !");
         window.location.reload()
@@ -202,14 +161,8 @@ function panierArticle(article) {
   }
 }
 /******************** Validation du formulaire  *************************/
-
 // On récupère le bouton commander
 let form = document.querySelector(".cart__order__form")
-// Champs prénom
-let prenom = document.getElementById("firstName");
-prenom.addEventListener("input", function(){
-  validPrenom(this)
-})
 // Fonction validPrenom
 const validPrenom = function(inputPrenom){
   let prenomRegExp = new RegExp ('^[a-zA-Zàâéèëêïîôùüç -]{2,20}$') 
@@ -226,14 +179,9 @@ const validPrenom = function(inputPrenom){
     return false
   }
 }
-// Champs nom
-let nom = document.getElementById("lastName");
-  nom.addEventListener("input", function(){
-  validName(this)
-})
 // Fonction validNom 
 const validName = function(inputName){
-  let nameRegExp = new RegExp ('^[a-zA-Zàâéèëêïîôùüç -]{2,60}$');
+  let nameRegExp = new RegExp ('^[a-zA-Zàâéèëêïîôùüç -]{2,30}$');
   let nameInfo = document.getElementById("lastNameErrorMsg");
   if(nameRegExp.test(inputName.value)){
     nameInfo.textContent = "";
@@ -242,24 +190,19 @@ const validName = function(inputName){
     nameInfo.textContent = "Champs obligatoire"
     return false
   } 
-   else{
+  else{
     nameInfo.textContent = "Nom incorrect!";
     return false
   }
 }
-// Champs adresse
-let adresse = document.getElementById("address");
-adresse.addEventListener("input", function(){
-  validAdress(this)
-})
 // Fonction validAdress
 const validAdress = function(inputAdress){
-  let adressRegExp = new RegExp ("^[a-zA-Z0-9\s,.' -]{3,}$");
+  let adressRegExp = new RegExp (`^[0-9a-z'àâéèêôùûçÀÂÉÈÔÙÛÇ\s,.' -]{1,50}$`);
   let adressInfo = document.getElementById("addressErrorMsg")
   if(adressRegExp.test(inputAdress.value)){
     adressInfo.textContent = "";
     return true
-  } else if(inputAdress.value < 1){
+  }else if(inputAdress.value < 1){
     adressInfo.textContent = "Champs obligatoire"
     return false
   } 
@@ -268,14 +211,9 @@ const validAdress = function(inputAdress){
     return false
   }
 }
-// Champs ville 
-let city = document.getElementById("city");
-city.addEventListener("input", function(){
-  validCity(this)
-});
 // Fonction validCity
 const validCity = function(inputCity){
-  let cityRegExp = new RegExp ('[a-z0-9._-]{3,12}$'); 
+  let cityRegExp = new RegExp ('^[a-zA-Zàâéèëêïîôùüç -]{2,30}$');
   let cityInfo = document.getElementById("cityErrorMsg");
   if(cityRegExp.test(inputCity.value)){
     cityInfo.textContent = ""
@@ -289,14 +227,9 @@ const validCity = function(inputCity){
     return false
   }
 }
-// Champs email
-let email = document.getElementById("email");
-email.addEventListener("input", function(){
-  validEmail(this);
-})
 // Fonction validEmail
 const validEmail = function(inputEmail){
-  let emailRegExp = new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$','g');
+  let emailRegExp = new RegExp('^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$', "g");
   let emailInfo = document.getElementById("emailErrorMsg");
   if(emailRegExp.test(inputEmail.value)){
     emailInfo.textContent = ""
@@ -310,16 +243,43 @@ const validEmail = function(inputEmail){
     return false
   }
 }
+function validForm(){
+// Champs prénom
+  let prenom = document.getElementById("firstName");
+  prenom.addEventListener("input", function(){
+    validPrenom(this)
+  })
+// Champs nom
+  let nom = document.getElementById("lastName");
+    nom.addEventListener("input", function(){
+    validName(this)  
+  })
+// Champs adresse  
+  let adresse = document.getElementById("address");
+  adresse.addEventListener("input", function(){
+    validAdress(this)
+  })
+// Champs ville 
+  let city = document.getElementById("city");
+  city.addEventListener("input", function(){
+    validCity(this)
+  });
+// Champs email
+  let email = document.getElementById("email");
+  email.addEventListener("input", function(){
+    validEmail(this);
+  })
+}
+function sendData(){
 // déclaration de la vairable du bouton commander
-let btnValider = document.getElementById("order");
+  let btnValider = document.getElementById("order");
 // On écoute le bouton "Commander"
-btnValider.addEventListener("click", function(event) {
-  event.preventDefault();
-  if(validPrenom(form.firstName) && validName(form.lastName) && validAdress(form.address) && validCity(form.city) && validEmail(form.email)){
-
-    let tabProduit = [];
-    let addProduits = JSON.parse(localStorage.getItem("produit"));
-    if (addProduits === null){
+  btnValider.addEventListener("click", function(event){
+    event.preventDefault();
+    if(validPrenom(form.firstName) && validName(form.lastName) && validAdress(form.address) && validCity(form.city) && validEmail(form.email)){ 
+      let tabProduit = [];
+      let addProduits = JSON.parse(localStorage.getItem("produit"));
+    if(addProduits === null){
       alert("Votre panier est vide")
     }
     for(let i = 0; i < addProduits.length; i++){
@@ -329,22 +289,21 @@ btnValider.addEventListener("click", function(event) {
       contact : {
         firstName: form.firstName.value,
         lastName: form.lastName.value,
-        address: form.address.value, // adresse 
+        address: form.address.value,
         city: form.city.value,
         email: form.email.value,
       },
       products: tabProduit,
     }
-
     const options = {
       method: 'POST',
       body: JSON.stringify(order),
       headers: {
         'Accept': 'application/json',
         "Content-Type": "application/json"
-      },
+      }
     };
-
+// envoie des données avec la méthode POST
     fetch("http://localhost:3000/api/products/order", options)
     .then((response) => response.json())
     .then((data) => {
@@ -355,42 +314,18 @@ btnValider.addEventListener("click", function(event) {
     }); 
 // si tout est ok, le local storage est vidée après la commande
     localStorage.clear();
-  } 
-  else {
+    } else{
     alert("Veuillez remplir les champs correctements avant de procéder au paiement");
-  }
-});
-
-// Fonction main qui regroupe les autres
-async function main(){  
-  const article = await getArticle()
-  panierArticle(article)
-} main()
-
-// au cas ou 
-/*
-function teste(){
-  carteProduit[i].addEventListener("change", function(e){
-
-    let teste = contenueQuantite[i].value;
-    let parse = parseInt(teste);
-    tabQuantite.splice(i, 1, parse)
-    const reducer3 = (accumulator, currentValue) => accumulator + currentValue;
-    const quantiteToto2 = tabQuantite.reduce(reducer3, 0);
-  // Total de la quantité afficher dans la page panier
-  totalProduits.textContent = quantiteToto2;  
-  
-  let sisi = parse * nomber;
-    ////////// PRIX QUAND IL CHANGE
-  let contenuePrix = document.querySelectorAll(".cart__item__content__description p:last-child");
-  contenuePrix[i].textContent = sisi + "€"
-  
-    let prix = contenuePrix[i].textContent;
-    let voila = parseInt(prix);
-    tabPrix.splice(i,1, voila)
-    const reducer4 = (accumulator, currentValue) => accumulator + currentValue;
-    const prixToto4 = tabPrix.reduce(reducer4, 0);
-  // Total du prix afficher dans la page panier
-    totalPrix.textContent = prixToto4;
+    }
   });
-}teste()*/
+}
+// Fonction main qui regroupe toutes les fonctions
+async function main(){  
+  const article = await getArticle();
+  displayArticle(article);
+  changeQuantityLocalStorage();
+  deletProduct();
+  calculTotalQuantitePrix();
+  validForm()
+  sendData()
+} main()
--- front/js/product.js
+++ front/js/product.js
@@ -3,7 +3,6 @@
 function getId(){ 
   return new URL(location.href).searchParams.get("id") 
 }
-
 // On récupère les données via fetch
 function getArticle(articleId){ 
   return fetch(`http://localhost:3000/api/products/${articleId}`)                           
@@ -19,7 +18,6 @@ function getArticle(articleId){
     alert("le serveur est eteint") 
   })
 }
-
 // On hydrate le contenue de la page produit avec les bonnes valeurs de l'article
 function hydrateArticle(article){  
 document.querySelector(".item article >.item__img").innerHTML = `<img src="${article.imageUrl}" alt="${article.altTxt}">`
@@ -33,112 +31,90 @@ let colorChoice = article.colors;
 for (let i = 0; i < colorChoice.length; i++){
 selectColor.innerHTML += `<option value="${colorChoice[i]}">${colorChoice[i]}</option>`;
 }
-
 /**************************LOCAL STORAGE *****************************/
-
 function event(){  
 // Selection des élements dans le DOM 
 let btnOrder = document.getElementById("addToCart");
 let color = document.querySelector('select[name="color-select"]');
 let quant = document.getElementById("quantity");
-
 // Écoute du changement de couleur
 color.addEventListener("change", function(c){
 tab.colors = c.target.value;
 })
-
 // Écoute du changement de la quantité
 quant.addEventListener("input", function(d){
 tab.quantite = d.target.value;
 })
-
 // Tableux des valeurs séléctionnées par l'utilisateur, affiché dans le localStorage
 let tab = {
 id :article._id, 
-colors : article.colors,  
+colors : article.colors,
 quantite: 1,
 };
-
-
 // Écoute du bouton " Ajouter au panier " 
 btnOrder.addEventListener("click", function(e){    
 // Si l'utilisateur oublie de renseigner une couleur
-if(tab.colors === article.colors || tab.colors === ""){    
-alert("Veillez sélectionner une couleur")
-sessionStorage.removeItem("produit"); 
-} else if( quant.value < 1 || quant.value > 100){
+  if(tab.colors === article.colors || tab.colors === ""){
+    alert("Veillez sélectionner une couleur")
+//sessionStorage.removeItem("produit"); // lOCAL STORAGE
+  } else if( quant.value < 1 || quant.value > 100){
   alert(" La quantité renseignée doit être comprise entre 1 et 100")
-} 
-else {
+  } else{
 // Fonction pour rediriger l'utilisateur en fonction de sa réponse
-function redirection(){
+  function redirection(){
     alert("votre produit à bien été ajouté au panier !")
-    // Si oui, il sera rdirigé vers la page panier 
-     let yes = confirm("Voulez-vous être redirigé vers la page panier ?")
-     if (yes){
+// Si oui, il sera rdirigé vers la page panier 
+    let yes = confirm("Voulez-vous être redirigé vers la page panier ?")
+    if (yes){
       document.location.href = "./cart.html"
-     } 
-}
-
-/*********** Envoie des produits selectionnés dans le local storage ****************/
-
-// On créer la variable qui contient les données du local storage 
-let produitJson = JSON.parse(localStorage.getItem("produit")); 
-// Première condition, si le localStorage est vide
-if(produitJson === null){
-produitJson = [];
-// On push les données ( du tableau créer plus haut ) dans le localStorage
-produitJson.push(tab);
-// On re actualise le localStorage (avec les nouvelles valeurs ajoutées)
-localStorage.setItem("produit", JSON.stringify(produitJson));
-redirection()
-} 
-
-// Deuxième condition, si le localStorage n'est pas vide
-else if(produitJson != null){
-// On boucle sur les données présente dans le localStorage
-for(let i = 0; i < produitJson.length; i++){
-  
-// Conversion en nombre de la quantité choisit par l'utilisateur et celle déja enregistrée dans le local storage
-  let valueQuantite = parseInt(tab.quantite);
-  let valueQuantiteLocalStorage = parseInt(produitJson[i].quantite);
-
-// si la quantité du produit est déja à 100 produits ajoutés dans le local storage 
-   if (produitJson[i].id === tab.id && produitJson[i].colors === tab.colors && produitJson[i].quantite >= 100){
-    alert("Vous ne pouvez pas ajouter plus de 100 produits du même type !")
-    return(
-      produitJson[i].quantite = valueQuantiteLocalStorage,
-      localStorage.setItem("produit", JSON.stringify(produitJson)))
-    }
-
-// Si les données qu'on ajoute sont égales aux données déjà enregistrées dans le localStorage 
- if (produitJson[i].id === tab.id && produitJson[i].colors === tab.colors){
-  return(
-    produitJson[i].quantite = valueQuantite + valueQuantiteLocalStorage,
-    localStorage.setItem("produit", JSON.stringify(produitJson))),
-    redirection()
- } 
-}
-// Si les données qu'on ajoute ont le même id mais pas la même couleur ou l'inverse
-for(let i = 0; i < produitJson.length; i++) {                 
-  if((produitJson[i].id === tab.id &&  produitJson[i].colors !== tab.colors) || produitJson[i].id !== tab.id ){
-// On retourne les valeurs ajoutées dans un nouvel index      
-    return(
-      produitJson.push(tab), 
-      localStorage.setItem("produit", JSON.stringify(produitJson))),
-      redirection()
+    } 
   }
-}
-}
-}
-// Fin du else (Dans le cas ou mon localStorage ne serait pas vide)
-
+/*********** Envoie des produits selectionnés dans le local storage ****************/
+// On créer la variable qui contient les données du local storage 
+  let produitJson = JSON.parse(localStorage.getItem("produit")); 
+  if(produitJson === null){
+  produitJson = [];
+// On push les données ( du tableau créer plus haut ) dans le localStorage
+  produitJson.push(tab);
+// On re actualise le localStorage (avec les nouvelles valeurs ajoutées)
+  localStorage.setItem("produit", JSON.stringify(produitJson));
+  redirection()
+  } else if(produitJson !== null){ 
+// On boucle sur les données présente dans le localStorage
+  for(let i = 0; i < produitJson.length; i++){
+// Conversion en nombre de la quantité choisit par l'utilisateur et celle déja enregistrée dans le local storage
+    let valueQuantite = parseInt(tab.quantite);
+    let valueQuantiteLocalStorage = parseInt(produitJson[i].quantite);
+// si la quantité du produit est déja à 100 produits ajoutés dans le local storage 
+    if (produitJson[i].id === tab.id && produitJson[i].colors === tab.colors && produitJson[i].quantite >= 100){
+      alert("Vous ne pouvez pas ajouter plus de 100 produits du même type !")
+      return(
+        produitJson[i].quantite = valueQuantiteLocalStorage,
+        localStorage.setItem("produit", JSON.stringify(produitJson)))
+      }
+// Si les données qu'on ajoute sont égales aux données déjà enregistrées dans le localStorage 
+    if (produitJson[i].id === tab.id && produitJson[i].colors === tab.colors){
+      return(
+        produitJson[i].quantite = valueQuantite + valueQuantiteLocalStorage,
+        localStorage.setItem("produit", JSON.stringify(produitJson))),
+        redirection()
+    } 
+  }
+// Si les données qu'on ajoute ont le même id mais pas la même couleur ou l'inverse
+  for(let i = 0; i < produitJson.length; i++) {                 
+    if((produitJson[i].id === tab.id &&  produitJson[i].colors !== tab.colors) || produitJson[i].id !== tab.id ){
+// On retourne les valeurs ajoutées dans un nouvel index      
+      return(
+        produitJson.push(tab), 
+        localStorage.setItem("produit", JSON.stringify(produitJson))),
+        redirection()
+    }
+  }
+  }
+  }
 });
-// Fin de l'écoute au click du bouton "ajouter au panier"
-
 }event()
 } //fin de la fonction HydrateArticle
-
 
 // Appelle de la fonction qui englobe tout 
 async function main(){  
